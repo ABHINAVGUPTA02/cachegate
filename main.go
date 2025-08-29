82480b2eba684fbc19745f47db10deb0be5b1a82
ABHINAVGUPTA02 <abhinav.gupta.dev05@gmail.com>
2025-08-30 02:18:48 +0530

    command taken from terminal and validated

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// take the string from the terminal
+	reader := bufio.NewReader(os.Stdin)
+	cmd, _ := reader.ReadString('\n') // Reads until Enter is pressed
+	cmd = strings.TrimSpace(cmd)
+	// store the string as a slice, using a delimiter " "
+	args := strings.Split(cmd, " ")
+	// validating the command
+	err := validateCommand(args)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// figuring out the command type
+	cmdType := commandType(args)
+	// based on the type of command we will perform the necessary actions
+	fmt.Println(cmdType)
+	if cmdType == 0 {
+		fmt.Println("clear cache command")
+	} else {
+		fmt.Println("starting the proxy command")
+	}
 }
