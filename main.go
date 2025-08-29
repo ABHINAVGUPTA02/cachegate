f5edb5f2dafd0695f25b4fb9d8edd332606bbc85
ABHINAVGUPTA02 <abhinav.gupta.dev05@gmail.com>
2025-08-30 02:34:22 +0530

    starting proxy server and clear cache implemented

--- main.go
+++ main.go
@@ -7,26 +7,32 @@ import (
 	"strings"
 )
 
+var portNum string
+
 func main() {
-	// take the string from the terminal
 	reader := bufio.NewReader(os.Stdin)
-	cmd, _ := reader.ReadString('\n') // Reads until Enter is pressed
-	cmd = strings.TrimSpace(cmd)
-	// store the string as a slice, using a delimiter " "
-	args := strings.Split(cmd, " ")
-	// validating the command
-	err := validateCommand(args)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	// figuring out the command type
-	cmdType := commandType(args)
-	// based on the type of command we will perform the necessary actions
-	fmt.Println(cmdType)
-	if cmdType == 0 {
-		fmt.Println("clear cache command")
-	} else {
-		fmt.Println("starting the proxy command")
+	for {
+		// take the string from the terminal
+		cmd, _ := reader.ReadString('\n') // Reads until Enter is pressed
+		cmd = strings.TrimSpace(cmd)
+		// store the string as a slice, using a delimiter " "
+		args := strings.Split(cmd, " ")
+		// validating the command
+		err := validateCommand(args)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// figuring out the command type
+		cmdType := commandType(args)
+		// based on the type of command we will perform the necessary actions
+		if cmdType == 0 {
+			restartServer(":" + portNum)
+			fmt.Println("clear cache command")
+		} else {
+			portNum = args[2]
+			startServer(":" + portNum)
+			fmt.Println("starting the proxy command")
+		}
 	}
 }
